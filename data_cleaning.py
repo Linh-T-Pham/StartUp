aa65dc2f3efef5968ceefa24457c732e66a4b2dd
Linh Pham <thuytpham9@gmail.com>
2020-03-26 16:29:37 -0700

    Finding data type in pandas

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,2 +1,48 @@
 import pandas as pd
-df = pd.read.csv('investments_VC.csv')
+df = pd.read_csv('investments_VC.csv', encoding='unicode_escape')
+
+
+# Data columns (total 39 columns):
+#  #   Column                Non-Null Count  Dtype  
+# ---  ------                --------------  -----  
+#  0   permalink             49438 non-null  object 
+#  1   name                  49437 non-null  object 
+#  2   homepage_url          45989 non-null  object 
+#  3   category_list         45477 non-null  object 
+#  4    market               45470 non-null  object 
+#  5    funding_total_usd    49438 non-null  object 
+#  6   status                48124 non-null  object 
+#  7   country_code          44165 non-null  object 
+#  8   state_code            30161 non-null  object 
+#  9   region                44165 non-null  object 
+#  10  city                  43322 non-null  object 
+#  11  funding_rounds        49438 non-null  int64  
+#  12  founded_at            38554 non-null  object 
+#  13  founded_month         38482 non-null  object 
+#  14  founded_quarter       38482 non-null  object 
+#  15  founded_year          38482 non-null  float64
+#  16  first_funding_at      49438 non-null  object 
+#  17  last_funding_at       49438 non-null  object 
+#  18  seed                  49438 non-null  int64  
+#  19  venture               49438 non-null  int64  
+#  20  equity_crowdfunding   49438 non-null  int64  
+#  21  undisclosed           49438 non-null  int64  
+#  22  convertible_note      49438 non-null  int64  
+#  23  debt_financing        49438 non-null  int64  
+#  24  angel                 49438 non-null  int64  
+#  25  grant                 49438 non-null  int64  
+#  26  private_equity        49438 non-null  int64  
+#  27  post_ipo_equity       49438 non-null  int64  
+#  28  post_ipo_debt         49438 non-null  int64  
+#  29  secondary_market      49438 non-null  int64  
+#  30  product_crowdfunding  49438 non-null  int64  
+#  31  round_A               49438 non-null  int64  
+#  32  round_B               49438 non-null  int64  
+#  33  round_C               49438 non-null  int64  
+#  34  round_D               49438 non-null  int64  
+#  35  round_E               49438 non-null  int64  
+#  36  round_F               49438 non-null  int64  
+#  37  round_G               49438 non-null  int64  
+#  38  round_H               49438 non-null  int64  
+# dtypes: float64(1), int64(22), object(16)
+# memory usage: 14.7+ MB
