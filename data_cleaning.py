6feef8fe7f5bbae26b016d6a4b4dc33e9870cc55
Linh Pham <thuytpham9@gmail.com>
2020-04-06 20:44:31 -0700

    Updated new_df

--- data_cleaning.py
+++ data_cleaning.py
@@ -100,15 +100,17 @@ Total_missing_values = df.isnull().sum().sum()
 old_df = df.shape  # The original dataset has 49438 rows and 39 columns 
 
 new_df = df.dropna()
-result_new_df = new_df.shape # After using the dropna to drop the columns, which have missing values. 
-                             # The new dataset has 21840 rows and 9 columns 
+# After using the dropna to drop the rows, which have missing values. 
+# The new dataset has 21840 rows and 39 columns 
 
 """"Drop columns in the new dataset"""
-new_df.drop(['permalink','category_list','founded_quarter','state_code', 'region','seed', 'venture', 'equity_crowdfunding',
+new_df.drop(['name', 'permalink','category_list','founded_quarter','state_code','seed', 'venture', 'equity_crowdfunding',
        'undisclosed', 'convertible_note', 'debt_financing', 'angel', 'grant',
        'private_equity', 'post_ipo_equity', 'post_ipo_debt',
        'secondary_market', 'product_crowdfunding', 'round_A', 'round_B',
        'round_C', 'round_D', 'round_E', 'round_F', 'round_G', 'round_H'], axis=1, inplace = True)
+# After dropping the columns, the new_df has 21840 rows and 13 columns 
+
 
 
 
